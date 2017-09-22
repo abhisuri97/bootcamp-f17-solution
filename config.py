fa7f37a84f00c2aabae97463bbc4dbfd8cb2b32a
Abhi <suriabhinav1997@gmail.com>
2017-09-22 01:34:55 -0400

    syntax

--- config.py
+++ config.py
@@ -3,9 +3,9 @@ import os
 DEBUG = True
 
 BASE_DIR = os.path.abspath(os.path.dirname(__file__))
-if os.environ['DATABASE_URL']
+if os.environ['DATABASE_URL']:
     SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
-else
+else:
     SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(BASE_DIR, 'app.db')
 SQLALCHEMY_TRACK_MODIFICATIONS = False
 
